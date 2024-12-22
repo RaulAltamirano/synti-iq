5a5a4133c4433dc6849f0d8cfb630f804c054495
Pacman <rulomania2011@hotmail.com>
2024-12-21 21:31:48 -0600

    create(get-user) add new decorator to auth

---
+++ src/auth/decorator/get-user.decorator.ts
@@ -0,0 +1,18 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    const user = req.user;
+    if (!user) {
+      throw new InternalServerErrorException(
+        'User object not found in request. Ensure AuthGuard is applied.',
+      );
+    }
+    return data ? user?.[data] : user;
+  },
+);
