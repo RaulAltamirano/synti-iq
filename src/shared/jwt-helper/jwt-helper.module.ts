64ac7b1e641c9908926a72a616d7706a2f2e6d8d
Pacman <rulomania2011@hotmail.com>
2024-12-31 10:07:59 -0600

    create(jwt-helper) add new hehelper to jet

--- src/shared/jwt-helper/jwt-helper.module.ts
+++ src/shared/jwt-helper/jwt-helper.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
 import { JwtHelperService } from './jwt-helper.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [ConfigModule],
-  providers: [JwtHelperService],
-  exports: [JwtHelperService],
+  imports: [ConfigModule, TypeOrmModule.forFeature([User])],
+  providers: [JwtHelperService, JwtStrategy],
+  exports: [JwtHelperService, JwtStrategy],
 })
 export class JwtHelperModule {}
