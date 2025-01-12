306c974b677013befd12eeed599809de785df34c
Pacman <rulomania2011@hotmail.com>
2025-01-11 20:45:47 -0600

    create(userRole) create structure to roles

---
+++ src/user-role/user-role.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { UserRoleService } from './user-role.service';
+import { UserRoleController } from './user-role.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRole } from './entities/user-role.entity';
+import { RoleModule } from 'src/role/role.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserRole]), RoleModule],
+  controllers: [UserRoleController],
+  providers: [UserRoleService],
+  // exports: [UserRoleService],
+})
+export class UserRoleModule {}
