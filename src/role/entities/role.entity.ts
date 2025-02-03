bedaa7c080011bb247dd5b0a5cbf1b42bccb90a7
Pacman <rulomania2011@hotmail.com>
2025-02-02 22:04:46 -0600

    create(rolEntity) add new entity

---
+++ src/role/entities/role.entity.ts
@@ -0,0 +1,34 @@
+import { PermissionGroup } from 'src/permission-group/entities/permission-group.entity';
+import { UserRole } from 'src/user-role/entities/user-role.entity';
+import { User } from 'src/user/entities/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+  OneToMany,
+} from 'typeorm';
+
+@Entity('roles')
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @ManyToMany(() => PermissionGroup, { cascade: true })
+  @JoinTable({
+    name: 'role_groups',
+    joinColumn: { name: 'role_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'group_id', referencedColumnName: 'id' },
+  })
+  permissionGroups: PermissionGroup[];
+
+  @OneToMany(() => UserRole, (userRole) => userRole.role)
+  users: User[];
+}
