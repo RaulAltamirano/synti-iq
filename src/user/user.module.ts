0bdbc82c75d0701180bbecc14c29a2893428d30e
Pacman <rulomania2011@hotmail.com>
2025-02-04 23:13:11 -0600

    create(userModule) add new module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,20 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { DatabaseModule } from 'src/database/database.module';
+import { PasswordModule } from 'src/auth/services/password/password.module';
+// import { AuthModule } from 'src/auth/services/auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [DatabaseModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    // AuthModule,
+    PasswordModule,
+    DatabaseModule,
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   exports: [UserService, TypeOrmModule],
 })
 export class UserModule {}
