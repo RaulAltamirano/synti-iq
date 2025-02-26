3ae1c2a979aec3feb6aba14f2522a7a7b8f4b743
Pacman <rulomania2011@hotmail.com>
2025-02-25 22:20:30 -0600

    update(userProfile) update user profile entity

---
+++ src/user_profile/entities/user_profile.entity.ts
@@ -0,0 +1,19 @@
+import { User } from 'src/user/entities/user.entity';
+import {
+  Entity,
+  TableInheritance,
+  PrimaryGeneratedColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity('user_profiles')
+@TableInheritance({ column: { type: 'varchar', name: 'type' } })
+export abstract class UserProfile {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @OneToOne(() => User, (user) => user.profile, { onDelete: 'CASCADE' })
+  @JoinColumn()
+  user: User;
+}
