70b4ac86b0ac0fb2c79276a3862a77a1e602bb57
Pacman <rulomania2011@hotmail.com>
2025-05-01 21:51:41 -0600

    create(session-filter) create new fto

---
+++ src/auth/dto/session-filter.dto.ts
@@ -0,0 +1,17 @@
+import { Type } from 'class-transformer';
+import { IsOptional, IsBoolean, IsDateString } from 'class-validator';
+import { BasePaginationParams } from 'src/pagination/dtos/base-pagination-params';
+
+export class SessionFilterDto extends BasePaginationParams {
+  @IsOptional()
+  @IsBoolean()
+  isValid?: boolean;
+
+  @IsOptional()
+  @IsDateString()
+  lastUsedAfter?: string;
+
+  @IsOptional()
+  @IsDateString()
+  lastUsedBefore?: string;
+} 
