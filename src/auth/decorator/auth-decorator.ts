7fa2f2d650e893fdda3b0393ccac4af3acae307f
Pacman <rulomania2011@hotmail.com>
2025-05-01 21:51:26 -0600

    update(auth-decorator) change decorator

--- src/auth/decorator/auth-decorator.ts
+++ src/auth/decorator/auth-decorator.ts
@@ -1,14 +1,14 @@
 // import { Role } from 'src/user/enums/roles.enum';
 // import { Permission } from 'src/user/enums/permissions.enum';
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 export const META_ROLES = 'roles';
 export const META_PERMISSIONS = 'permissions';
 
 export function Auth(roles: string[] = [], permissions: string[] = []) {
   return applyDecorators(
-    UseGuards(AuthGuard()),
+    UseGuards(JwtAuthGuard),
     SetMetadata(META_ROLES, roles),
     SetMetadata(META_PERMISSIONS, permissions),
   );
