5e48a5ed7da210f56ec7e3a67a738f3c78d812e9
Pacman <rulomania2011@hotmail.com>
2025-06-16 22:15:14 -0600

    create(update-assignment-dto) introduce UpdateAssignmentDto for updating assignment details with validation rules for start time, end time, cashier ID, max assignments, and future block updates

---
+++ src/cashier-schedule-assignment/dto/update-assignment.dto.ts
@@ -0,0 +1,36 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsOptional, IsDateString, IsUUID, IsInt, Min, IsBoolean } from 'class-validator';
+
+@InputType()
+export class UpdateAssignmentDto {
+  @Field(() => Date, { nullable: true })
+  @IsOptional()
+  @IsDateString()
+  startTime?: Date;
+
+  @Field(() => Date, { nullable: true })
+  @IsOptional()
+  @IsDateString()
+  endTime?: Date;
+
+  @Field(() => String, { nullable: true })
+  @IsOptional()
+  @IsUUID()
+  cashierId?: string;
+
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  maxAssignments?: number;
+
+  @Field(() => Boolean, { nullable: true })
+  @IsOptional()
+  @IsBoolean()
+  updateFutureBlocks?: boolean;
+
+  @Field(() => Date, { nullable: true })
+  @IsOptional()
+  @IsDateString()
+  updateFromDate?: Date;
+} 
