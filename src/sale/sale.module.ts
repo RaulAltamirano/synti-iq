3a357db08704b3ad469134e4e27b9d84993c00c6
Pacman <rulomania2011@hotmail.com>
2026-04-07 23:46:51 -0600

    fix(sale): register Sale entity in SaleModule forFeature

--- src/sale/sale.module.ts
+++ src/sale/sale.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SaleService } from './sale.service';
 import { SaleController } from './sale.controller';
+import { Sale } from './entities/sale.entity';
 import { SaleItem } from 'src/sale-item/entities/sale-item.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from 'src/transactions/entities/transaction.entity';
@@ -8,7 +9,7 @@ import { Transaction } from 'src/transactions/entities/transaction.entity';
 @Module({
   controllers: [SaleController],
   providers: [SaleService],
-  imports: [TypeOrmModule.forFeature([SaleItem, Transaction])],
+  imports: [TypeOrmModule.forFeature([Sale, SaleItem, Transaction])],
   exports: [TypeOrmModule, SaleService],
 })
 export class SaleModule {}
